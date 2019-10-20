7d485066e586d863721f5a9997693f153a3898dc
mbaridonn <mbaridonn@gmail.com>
2019-10-19 23:00:22 -0300

    Updated tp2 with excel reader class

--- tp2.py
+++ tp2.py
@@ -1,23 +1,46 @@
 import numpy as np
+import xlrd
+import pandas
 
-#testing data
-genero = 1
-edad = 22
-provincia = 1
-años_experiencia = 2
-nivel_estudios = 1
+class Excel_Reader(object):
+  def __init__(self):
+    df = pandas.read_excel('sample.xlsx')
+    self.training_values = df.values
 
-# X = (hours studying, hours sleeping), y = score on test
-xAll = np.array(([1,28,3,3,3], [2,30,1,5,4], [3,26,2,7,4], [genero, edad, provincia, años_experiencia, nivel_estudios]), dtype=float) # input data
-y = np.array(([80000], [120000], [110000]), dtype=float)
+  def apply_conditions(self):
+    self.training_values[self.training_values == 'Hombre'] = 1
+    self.training_values[self.training_values == 'Mujer'] = 2
+    self.training_values[self.training_values == 'Otros'] = 3
+    self.training_values[self.training_values == 'CABA'] = 1
+    self.training_values[self.training_values == 'GBA'] = 2
+    self.training_values[self.training_values == 'Catamarca'] = 3
+    self.training_values[self.training_values == 'Chaco'] = 4
+    self.training_values[self.training_values == 'Chubut'] = 5
+    self.training_values[self.training_values == 'Cordoba'] = 6
+    self.training_values[self.training_values == 'Corrientes'] = 7
+    self.training_values[self.training_values == 'Entre Rios'] = 8
+    self.training_values[self.training_values == 'Formosa'] = 9
+    self.training_values[self.training_values == 'Jujuy'] = 10
+    self.training_values[self.training_values == 'La Pampa'] = 11
+    self.training_values[self.training_values == 'La Rioja'] = 12
+    self.training_values[self.training_values == 'Mendoza'] = 13
+    self.training_values[self.training_values == 'Misiones'] = 14
+    self.training_values[self.training_values == 'Neuquen'] = 15
+    self.training_values[self.training_values == 'Rio Negro'] = 16
+    self.training_values[self.training_values == 'Salta'] = 17
+    self.training_values[self.training_values == 'San Juan'] = 18
+    self.training_values[self.training_values == 'San Luis'] = 19
+    self.training_values[self.training_values == 'Santa Cruz'] = 20
+    self.training_values[self.training_values == 'Santa Fe'] = 21
+    self.training_values[self.training_values == 'Santiago del Estero'] = 22
+    self.training_values[self.training_values == 'Tierra del Fuego'] = 23
+    self.training_values[self.training_values == 'Secundario'] = 1
+    self.training_values[self.training_values == 'Terciario'] = 2
+    self.training_values[self.training_values == 'Universitario'] = 3
+    self.training_values[self.training_values == 'Posgrado'] = 4
 
-# scale units
-xAll = xAll/np.amax(xAll, axis=0) # scaling input data
-y = y/np.amax(y, axis=0) # scaling output data
-
-# split data
-X = np.split(xAll, [3])[0] # training data
-xPredicted = np.split(xAll, [3])[1] # testing data
+  def get_training_values(self):
+    return self.training_values
 
 class Neural_Network(object):
   def __init__(self):
@@ -72,6 +95,31 @@ class Neural_Network(object):
     #print ("Output: \n" + str(self.forward(xPredicted)))
     print ("Output: \n" + str(self.forward(xPredicted)[0][0] * 110000))
 
+
+
+#testing data
+genero = 1
+edad = 22
+provincia = 1
+años_experiencia = 2
+nivel_estudios = 1
+
+#read excel
+
+# X = (hours studying, hours sleeping), y = score on test
+xAll = np.array(([1,28,3,3,3], [2,30,1,5,4], [3,26,2,7,4], [genero, edad, provincia, años_experiencia, nivel_estudios]), dtype=float) # input data
+y = np.array(([80000], [120000], [110000]), dtype=float)
+
+# scale units
+xAll = xAll/np.amax(xAll, axis=0) # scaling input data
+y = y/np.amax(y, axis=0) # scaling output data
+
+# split data
+X = np.split(xAll, [3])[0] # training data
+xPredicted = np.split(xAll, [3])[1] # testing data
+
+
+
 NN = Neural_Network()
 for i in range(1000): # trains the NN 1,000 times
   print ("# " + str(i) + "\n")
@@ -84,3 +132,21 @@ for i in range(1000): # trains the NN 1,000 times
 
 NN.saveWeights()
 NN.predict()
+
+
+excel_reader = Excel_Reader()
+excel_reader.apply_conditions()
+print("Training Values:")
+print(excel_reader.get_training_values())
+
+print("\n xAll:")
+print(xAll);
+print("\n prueba: ")
+prueba = excel_reader.get_training_values()
+row_to_add = np.array([genero, edad, provincia, años_experiencia, nivel_estudios])
+prueba = np.vstack((prueba,row_to_add))
+print(prueba)
+
+'''
+xAll = np.array(([1,28,3,3,3], [2,30,1,5,4], [3,26,2,7,4], [genero, edad, provincia, años_experiencia, nivel_estudios]), dtype=float) # input data
+'''
